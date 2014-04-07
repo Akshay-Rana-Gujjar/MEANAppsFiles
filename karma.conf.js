8d652804ccc595f4aefbc21da1487889ef7c7287
Guy Ellis <wildfiction@gmail.com>
2014-04-07 15:56:02 -0700

    At end of  "Testing the mvUser Service" in Module 7

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'public/vendor/angular/angular.js',
-      'public/vendor/angular-resources/angular-resource.js',
+      'public/vendor/angular-resource/angular-resource.js',
       'public/vendor/angular-mocks/angular-mocks.js',
 		  'test/test-app.js',
       'public/app/**/*.js',
