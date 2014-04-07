4917a501edf4f13736a4839ed22f0108c518a241
Guy Ellis <wildfiction@gmail.com>
2014-04-07 08:24:54 -0700

    At end of  "Creating the Server Login Code" in Module 5

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,23 @@
-
+var passport = require('passport');
 
 module.exports = function(app) {
 
 	app.get('/partials/*', function (req, res) {
 		res.render('../../public/app/' + req.params);
+	});
+
+	app.post('/login', function(req,res,next){
+		var auth = passport.authenicate('local',function(err,user){
+			if(err) {return next(err);}
+			if(!user) {res.send({success:false});}
+			// logIn() is a function that passport adds to the request object
+			// Normally don't need to tell passport to login because it does that automatically but here we're doing it through an Ajax POST
+			req.logIn(user, function(err){
+				if(err) {return next(err);}
+				res.send({success:true, user:user});
+			})
+		});
+		auth(req,res,next);
 	});
 
 	app.get('*', function (req, res) {
