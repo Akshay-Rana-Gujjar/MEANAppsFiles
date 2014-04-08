ea03253189d474720abd0db78bf8567dc4eae510
Guy Ellis <wildfiction@gmail.com>
2014-04-07 21:59:03 -0700

    At end of  "Caching Data" in Module 10

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvMainCtrl', function($scope, mvCourse){
-	$scope.courses = mvCourse.query();
+angular.module('app').controller('mvMainCtrl', function($scope, mvCachedCourse){
+	$scope.courses = mvCachedCourse.query();
 });
 
